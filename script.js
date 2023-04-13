94f1c0cd692612dd0645f6ffe98501edde2ea623
Jose Lima <jalimag0206@gmail.com>
2023-04-13 18:48:21 -0300

    arrumando bug botao tabs

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
-    $('#produtos button').click(function () {
+    $('.botao-carrinho').click(function () {
         const destino = $('#carrinho');
+        console.log("foi?")
 
         const nomeProduto = $(this).parent().find('h5').text();
         const valorProduto = $(this).parent().find('span').text();
